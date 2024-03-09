db56606e58ab731223fd7850fed927da7b65e945
LouisKien <louisnamu02@gmail.com>
2024-03-09 11:29:45 +0700

    fix policy

--- SWD_ICQS/Program.cs
+++ SWD_ICQS/Program.cs
@@ -41,12 +41,12 @@ builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.Al
 
 builder.Services.AddAuthorization(options =>
 {
-    options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("1"));
-    options.AddPolicy("RequireContractorRole", policy => policy.RequireRole("2"));
-    options.AddPolicy("RequireCustomerRole", policy => policy.RequireRole("3"));
-    options.AddPolicy("RequireAdminOrContractorRole", policy => policy.RequireRole("1", "2"));
-    options.AddPolicy("RequireAdminOrCustomerRole", policy => policy.RequireRole("1", "3"));
-    options.AddPolicy("RequireContractorOrCustomerRole", policy => policy.RequireRole("2", "3"));
+    options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("0"));
+    options.AddPolicy("RequireContractorRole", policy => policy.RequireRole("1"));
+    options.AddPolicy("RequireCustomerRole", policy => policy.RequireRole("2"));
+    options.AddPolicy("RequireAdminOrContractorRole", policy => policy.RequireRole("0", "1"));
+    options.AddPolicy("RequireAdminOrCustomerRole", policy => policy.RequireRole("0", "2"));
+    options.AddPolicy("RequireContractorOrCustomerRole", policy => policy.RequireRole("1", "2"));
 });
 var app = builder.Build();
 

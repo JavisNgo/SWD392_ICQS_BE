a164f18b841322e9fe3371ba6a0ab7440797e0e7
Louis Kien <louisnamu02@gmail.com>
2024-03-11 22:25:26 +0700

    customer and contractor crud

--- SWD_ICQS/ModelsView/ContractorsView.cs
+++ SWD_ICQS/ModelsView/ContractorsView.cs
@@ -2,10 +2,14 @@
 {
     public class ContractorsView
     {
+        public int Id { get; set; }
         public string? Name { get; set; }
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Address { get; set; }
         public byte[]? AvatarBin { get; set; }
+        public int SubscriptionId { get; set; }
+        public DateTime ExpiredDate { get; set; }
+        public int AccountId { get; set; }
     }
 }
